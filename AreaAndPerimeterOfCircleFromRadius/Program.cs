c033d8af2cf896bd99285c43cab751b5bd02dce1
Brett Griffin <9950230@student.toiohomai.ac.nz>
2019-02-25 16:43:52 +1300

    Initial Commit

---
+++ AreaAndPerimeterOfCircleFromRadius/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaAndPerimeterOfCircleFromRadius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a Number: ");
+            double radius = double.Parse(Console.ReadLine());
+
+            double pi = Math.PI;
+
+            // Perimeter = 2 * PI * r
+            double circlePerimeter = 2 * pi * radius;
+
+            // Area = PI * r^2
+            double circleArea = pi * Math.Pow(radius, 2);
+
+
+            Console.WriteLine("Perimeter: " + circlePerimeter);
+            Console.WriteLine("Area: " + circleArea);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProductOfTwoInputs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductOfTwoInputs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input first number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.Write("Input second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(firstNumber * secondNumber);
+            Console.ReadLine();
+
+        }
+    }
+}
