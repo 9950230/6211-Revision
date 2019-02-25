9daa0cdd8928edeb62d3f2818d22803df1a36e3e
Brett Griffin <9950230@student.toiohomai.ac.nz>
2019-02-25 16:52:31 +1300

    Added Project: Swap Two Variables

---
+++ SwapTwoVariables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapTwoVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valueOne = 10;
+            int valueTwo = 20;
+
+            Console.WriteLine("A = " + valueOne + "  B = " + valueTwo);
+            int temp = valueOne;
+            valueOne = valueTwo;
+            valueTwo = temp;
+
+            Console.WriteLine("A = " + valueOne + "  B = " + valueTwo);
+            Console.ReadLine();
+        }
+    }
+}
